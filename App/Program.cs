396fa27ceefdc644985290476f108faab0b20668
vekt0R-HUB <d0t42f4n@gmail.com>
2022-06-27 20:40:48 -0400

    Update

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using MathemathicExpresion;
+
+public class Program
+{
+    public static void Main()
+    {
+        var s = new Constant(4);
+        var t = new Variable('a');
+        var r = new Multiplication(s,t);
+
+        System.Console.WriteLine(r.DerivateInVariable().Symplify());
+    }
+}
