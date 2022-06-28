840a9855ad4eb18dc4cb29bd84ab72279d6b823b
vekt0R-HUB <d0t42f4n@gmail.com>
2022-06-27 20:51:19 -0400

    Update

--- App/Program.cs
+++ App/Program.cs
@@ -8,7 +8,8 @@ public class Program
         var s = new Constant(4);
         var t = new Variable('a');
         var r = new Multiplication(s,t);
+        var p = new Sin(r);
 
-        System.Console.WriteLine(r.DerivateInVariable().Symplify());
+        System.Console.WriteLine(p.DerivateInVariable().Symplify());
     }
 }
